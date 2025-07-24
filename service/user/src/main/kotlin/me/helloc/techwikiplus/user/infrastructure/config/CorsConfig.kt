b3ba79315b73aab69438b4997b3615cfeeaa8f31
leesh5000 <leesh5000@gmail.com>
2025-07-24 13:31:28 +0900

    CI/CD 테스트

--- service/user/src/main/kotlin/me/helloc/techwikiplus/user/infrastructure/config/CorsConfig.kt
+++ service/user/src/main/kotlin/me/helloc/techwikiplus/user/infrastructure/config/CorsConfig.kt
@@ -1,7 +1,6 @@
 package me.helloc.techwikiplus.user.infrastructure.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.cors.CorsConfiguration
@@ -9,15 +8,13 @@ import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @ConfigurationProperties(prefix = "cors")
-data class CorsProperties
-    @ConstructorBinding
-    constructor(
-        val allowedOrigins: List<String> = listOf("*"),
-        val allowedMethods: List<String> = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS"),
-        val allowedHeaders: List<String> = listOf("*"),
-        val allowCredentials: Boolean = true,
-        val maxAge: Long = 3600,
-    )
+data class CorsProperties(
+    val allowedOrigins: List<String> = listOf("*"),
+    val allowedMethods: List<String> = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS"),
+    val allowedHeaders: List<String> = listOf("*"),
+    val allowCredentials: Boolean = true,
+    val maxAge: Long = 3600,
+)
 
 @Configuration
 open class CorsConfig(
